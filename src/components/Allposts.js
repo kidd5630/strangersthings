eabfa3f79ede133ade10e46bbb8cd5340407ff0e
jsitinas94 <jsitinas94@gmail.com>
2021-08-20 15:12:42 -0400

    created allposts functionallity for unauthenticated user

--- src/components/Allposts.js
+++ src/components/Allposts.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState }from 'react';
 
-const Allposts = () => {
-    return <h1>All of dems</h1>
+import {
+    BASE_URL,
+    fetchAllPosts
+} from '../api';
+
+const Allposts = (props) => {
+
+    const [allPosts, setAllPosts] = useState([])
+    
+    useEffect(() => {
+        fetch(`${BASE_URL}/posts`)
+        .then(res => res.json())
+        .then(results => {setAllPosts(results.data.posts)})
+    }, [])
+
+    console.log(allPosts)
+
+    return (
+        <div id="allposts">
+         {allPosts.map(post => {
+           return <div key={post._id}>
+           <h3>{post.title}</h3>
+           <p>{post.description}</p>
+           <p>Price: {post.price}</p>
+           <p>Seller: {post.author.username}</p>
+           <p>Location: {post.location}</p>
+           </div>
+         })
+        }
+        </div>
+    )
+
+
 }
 
 
